bd59b5dbf41b33f10a1cc9b9e9ae6ac0239c6bdd
Martin Herbst <mail@mherbst.de>
2013-08-25 21:05:37 +0200

    Basisversion

--- plugin.php
+++ plugin.php
@@ -4,16 +4,16 @@ class MHerbstInsertThumbPlugin extends KokenPlugin {
 
     function __construct()
     {
-        $this->register_shortcode('mherbst_insertthumb', 'render');
+        $this->register_shortcode('mhthumbs', 'render');
     }
 
     function render($attributes)
     {
         return <<<HTML
-<div class="k-content-embed">
+<div class="k-content-embed" style="float:right">
     <koken:load source="content" filter:id="{$attributes['id']}">
         <div class="k-content">
-            <koken:img />
+            <koken:img preset="small" />
         </div>
     </koken:load>
 </div>
