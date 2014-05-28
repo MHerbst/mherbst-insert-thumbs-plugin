76908daa44813d2cfdff42532c0d231168b78708
Martin Herbst <mail@mherbst.de>
2014-05-28 19:14:32 +0200

    New option "crop" + others
    - New option "crop"
    - Modified CSS class according to Koken documentation

--- plugin.php
+++ plugin.php
@@ -2,80 +2,75 @@
 
 class MHerbstInsertThumbPlugin extends KokenPlugin {
 
-    function __construct()
-    {
-        $this->register_shortcode('mherbst_thumbs', 'render');
-    }
+	function __construct() {
+		$this->register_shortcode('mherbst_thumbs', 'render');
+	}
 
-    function render($attributes)
-    {
-    	$query = $_SERVER['QUERY_STRING'];
+	function render($attributes) 	{
+		$query = $_SERVER['QUERY_STRING'];
  /*   	$handle = fopen("/test.log","a");
     	fwrite($handle, ">>".$query."<<\n");
     	fwrite($handle, "StriPos:".stripos($query, "/text/slug:")."--".stripos($query, "/type:essay/")."\n");
     	fwrite($handle, "Stripos2:".stripos($query, "/type:page/"));
   */  	
 
-    	if ((stripos($query, "/text/slug:") === false && !$this->data->show_in_index) && stripos($query, "/type:page/") === false)
+		if ((stripos($query, "/text/slug:") === false && !$this->data->show_in_index) && stripos($query, "/type:page/") === false)
 			return "";
-    	 
-    	$class = $attributes['class'];
-    	$style="";
-        $margin = false;
-        $size = "";
-        if (is_numeric($attributes['margin']) && !empty($attributes['margin'])) {
-            $margin = $attributes['margin'];
-        }
-    	if ($attributes['floating'] == "l")
-    	{
-    		$style = 'style="float:left;';
-                if ($margin) {
-                    $style .= ' margin-right: ' . $margin . 'px;';
-                }
-                $style .= '"'; // close style attribute
-    	}
-    	else if ($attributes['floating'] == "r")
-    	{
-    		$style = 'style="float:right;';
-                if ($margin) {
-                    $style .= ' margin-left: ' . $margin . 'px;';
-                }
-                $style .= '"'; // close style attribute
-    	}
+
+		$class = $attributes['class'];
+		$style="";
+		$margin = false;
+		$size = "";
+		if (is_numeric($attributes['margin']) && !empty($attributes['margin'])) {
+			$margin = $attributes['margin'];
+		}
+		if ($attributes['floating'] == "l") {
+			$style = 'style="float:left;';
+			if ($margin) {
+				$style .= ' margin-right: ' . $margin . 'px;';
+			}
+			$style .= '"'; // close style attribute
+		} else if ($attributes['floating'] == "r") {
+			$style = 'style="float:right;';
+			if ($margin) {
+				$style .= ' margin-left: ' . $margin . 'px;';
+			}
+			$style .= '"'; // close style attribute
+		}
 
 		if (!empty($attributes['size'])) {
 			$size = ' size="' . $attributes['size'] . '"';
 		}
 
-    	$width = ($attributes['width'] != "") ? 'width="'.$attributes['width'].'"' : "";
-    	switch($attributes['preset']) 
-    	{
-    		case "t":
-    			$preset = 'preset="tiny"';
-    			break;
-    		case "s":
-    			$preset = 'preset="small"';
-    			break;
-    		case "m":
-    			$preset = 'preset="medium"';
-    			break;
-    		default:
-    			$preset = "";    				 
-    	}
-    	$lazy = 'lazy="true"';
-    	if ($preset == "" && $width == "")
-    		$lazy = "";
- 	
+		$width = ($attributes['width'] != "") ? 'width="'.$attributes['width'].'"' : "";
+		$crop =  ($attributes['crop'] == "true") ? 'crop="true"' : '';
+		switch($attributes['preset']) {
+			case "t":
+				$preset = 'preset="tiny"';
+				break;
+			case "s":
+				$preset = 'preset="small"';
+				break;
+			case "m":
+				$preset = 'preset="medium"';
+				break;
+			default:
+				$preset = "";
+		}
+		$lazy = 'lazy="true"';
+		if ($preset == "" && $width == "")
+			$lazy = "";
+
 		switch($attributes['caption'])
 		{
 			case "t":
-				$caption = "{{ content.title }}";
+				$caption = '<span class="k-content-title">{{ content.title }}</span>';
 				break;
 			case "c":
-				$caption = "{{ content.caption }}";
+				$caption = '<span class="k-content-caption">{{ content.caption }}</span>';
 				break;
 			case "b":
-				$caption = "{{ content.title }} - {{ content.caption }}";
+				$caption = '<span class="k-content-title">{{ content.title }}</span> - <span class="k-content-caption">{{ content.caption }}</span>';
 				break;
 			default:
 				$caption = "";
@@ -83,7 +78,7 @@ class MHerbstInsertThumbPlugin extends KokenPlugin {
 		}
 		if ($caption != "")
 		{
-			$caption = "<figcaption>".$caption."</figcaption>";
+			$caption = '<figcaption class="k-content-text">'.$caption.'</figcaption>';
 		}
 		
 		switch($attributes['link'])
@@ -106,18 +101,15 @@ class MHerbstInsertThumbPlugin extends KokenPlugin {
 						
 		}
 		$linkend = ($linkbegin != "") ? "</koken:link>" : "";
-        return <<<HTML
-<div class="k-content-embed {$class}" {$style}>
-    <koken:load source="content" filter:id="{$attributes['id']}">
-        <figure class="k-content">
-        		{$linkbegin}<koken:img {$size} {$preset} {$width} {$lazy} />{$linkend}
-        		{$caption}
-      	</figure>
-    </koken:load>
+		return <<<HTML
+<div class="k-content {$class}" {$style}>
+	<koken:load source="content" filter:id="{$attributes['id']}">
+		<figure class="k-content-embed">
+			{$linkbegin}<koken:img {$size} {$preset} {$width} {$lazy} {$crop} />{$linkend}
+			{$caption}
+		</figure>
+	</koken:load>
 </div>
 HTML;
-    }
-
+	}
 }
-
-
